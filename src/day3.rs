d2030ba9f0b6a3a9547300eae0dca4ee43b97030
Steven Reitsma <steven@properchaos.nl>
2020-12-06 16:22:15 +0100

    cargo fmt

--- src/day3.rs
+++ src/day3.rs
@@ -1,9 +1,12 @@
-use std::fs;
 use ndarray::Array2;
+use std::fs;
 
 pub fn get_input() -> Result<Array2<char>, ndarray::ShapeError> {
     let input = fs::read_to_string("input/day3.txt").expect("couldn't read input file");
-    return Array2::from_shape_vec((input.lines().count(), input.lines().nth(0).unwrap().len()), input.replace("\n", "").chars().collect());
+    return Array2::from_shape_vec(
+        (input.lines().count(), input.lines().nth(0).unwrap().len()),
+        input.replace("\n", "").chars().collect(),
+    );
 }
 
 pub fn a(input: Array2<char>, slope_x: usize, slope_y: usize) -> usize {
