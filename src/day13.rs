1fd27d203ebff7703025e42d993a41bf9238d8d4
Steven Reitsma <steven@properchaos.nl>
2020-12-13 11:12:01 +0100

    Day 13 - B

--- src/day13.rs
+++ src/day13.rs
@@ -8,7 +8,7 @@ pub fn get_input() -> (isize, Vec<isize>) {
         .lines()
         .nth(1)
         .unwrap()
-        .replace("x,", "")
+        .replace("x,", "0,")
         .split(",")
         .map(|x| x.parse().unwrap())
         .collect();
@@ -21,6 +21,10 @@ pub fn a(time: isize, buses: &Vec<isize>) -> isize {
     let mut waiting = 1.;
 
     for bus in buses {
+        if *bus == 0 {
+            continue;
+        }
+
         let ratio = time as f32 / *bus as f32;
         let remainder = ratio.ceil() - ratio;
 
@@ -35,6 +39,58 @@ pub fn a(time: isize, buses: &Vec<isize>) -> isize {
     return minutes * closest_bus;
 }
 
+
+// START from https://rosettacode.org/wiki/Chinese_remainder_theorem#Rust
+fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
+    if a == 0 {
+        (b, 0, 1)
+    } else {
+        let (g, x, y) = egcd(b % a, a);
+        (g, y - (b / a) * x, x)
+    }
+}
+
+fn mod_inv(x: i64, n: i64) -> Option<i64> {
+    let (g, x, _) = egcd(x, n);
+    if g == 1 {
+        Some((x % n + n) % n)
+    } else {
+        None
+    }
+}
+
+fn chinese_remainder(residues: Vec<i64>, modulii: Vec<i64>) -> Option<i64> {
+    let prod = modulii.iter().product::<i64>();
+
+    let mut sum = 0;
+
+    for (residue, modulus) in residues.iter().zip(modulii) {
+        let p = prod / modulus;
+        sum += residue * mod_inv(p, modulus)? * p
+    }
+
+    Some(sum % prod)
+}
+
+// END from https://rosettacode.org/wiki/Chinese_remainder_theorem#Rust
+
+pub fn b(buses: &Vec<isize>) -> i64 {
+    let modulii = buses
+        .iter()
+        .filter(|x| **x != 0)
+        .map(|x| *x as i64)
+        .collect();
+
+    let residues = buses
+        .iter()
+        .enumerate()
+        .filter(|&(_, x)| *x != 0)
+        .map(|(i, x)| (*x - i as isize) as i64)
+        .collect();
+
+    return chinese_remainder(residues, modulii).unwrap();
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -43,6 +99,13 @@ mod test {
     fn example_a() {
         let input = &get_input();
         let result = a(input.0, &input.1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 370);
+    }
+
+    #[test]
+    fn example_b() {
+        let input = &get_input();
+        let result = b(&input.1);
+        assert_eq!(result, 894954360381385);
     }
 }
