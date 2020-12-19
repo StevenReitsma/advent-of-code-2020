b66420271b2b8b0ba18607831e4968b868b56451
Steven Reitsma <steven@properchaos.nl>
2020-12-19 16:01:04 +0100

    Generalized day 17

--- src/day17.rs
+++ src/day17.rs
@@ -2,16 +2,41 @@ use ndarray::Array2;
 use std::collections::HashSet;
 use std::fs;
 
-type Coord3 = (isize, isize, isize);
-type Coord4 = (isize, isize, isize, isize);
+type Coord = Vec<isize>;
+
+pub trait NeighbourExt {
+    fn neighbours(&self) -> Vec<Coord>;
+}
+
+impl NeighbourExt for Vec<isize> {
+    fn neighbours(&self) -> Vec<Coord> {
+        let mut vec = Vec::<Coord>::new();
+        let dim = self.len();
+
+        if dim == 0 {
+            return vec![];
+        }
+
+        for x in -1..2 {
+            let n = vec![self[0] + x];
+
+            if dim == 1 {
+                vec.push(n.clone());
+            }
+
+            for neigh in self[1..].to_vec().neighbours() {
+                vec.push([n.clone(), neigh].concat());
+            }
+        }
+
+        return vec;
+    }
+}
 
 pub fn get_input() -> Result<Array2<usize>, ndarray::ShapeError> {
     let input = fs::read_to_string("input/day17.txt").expect("couldn't read input file");
     return Array2::from_shape_vec(
-        (
-            input.lines().count(),
-            input.lines().nth(0).unwrap().len()
-        ),
+        (input.lines().count(), input.lines().nth(0).unwrap().len()),
         input
             .replace("\n", "")
             .replace("#", "1")
@@ -22,74 +47,46 @@ pub fn get_input() -> Result<Array2<usize>, ndarray::ShapeError> {
     );
 }
 
-pub fn neighbours3(coord: &Coord3) -> Vec<Coord3> {
-    let mut vec = Vec::<Coord3>::new();
+pub fn a(input: &Array2<usize>, dims: usize) -> usize {
+    let base_vec = vec![0; dims - 2];
+    let mut active_cubes = input
+        .iter()
+        .enumerate()
+        .filter(|(_, value)| **value == 1)
+        .map(|(coord, _)| {
+            [
+                vec![
+                    coord as isize / input.shape()[1] as isize,
+                    coord as isize % input.shape()[1] as isize,
+                ],
+                base_vec.clone(),
+            ]
+            .concat()
+        })
+        .collect::<HashSet<Coord>>();
 
-    for x in coord.0 - 1..coord.0 + 2 {
-        for y in coord.1 - 1..coord.1 + 2 {
-            for z in coord.2 - 1..coord.2 + 2 {
-                if x == coord.0 && y == coord.1 && z == coord.2 {
+    for _ in 0..6 {
+        let mut next_active_cubes = HashSet::<Coord>::new();
+
+        for active in active_cubes.iter() {
+            let neighbour_cubes = active.neighbours();
+            let mut neighbour_cubes_active = 0;
+
+            for neighbour_cube in neighbour_cubes {
+                if neighbour_cube == *active {
                     continue;
                 }
-                vec.push((x, y, z));
-            }
-        }
-    }
 
-    return vec;
-}
-
-pub fn neighbours4(coord: &Coord4) -> Vec<Coord4> {
-    let mut vec = Vec::<Coord4>::new();
-
-    for x in coord.0 - 1..coord.0 + 2 {
-        for y in coord.1 - 1..coord.1 + 2 {
-            for z in coord.2 - 1..coord.2 + 2 {
-                for w in coord.3 - 1..coord.3 + 2 {
-                    if x == coord.0 && y == coord.1 && z == coord.2 && w == coord.3 {
-                        continue;
-                    }
-                    vec.push((x, y, z, w));
-                }
-            }
-        }
-    }
-
-    return vec;
-}
-
-pub fn a(input: &Array2<usize>) -> usize {
-    let mut active_cubes = input
-        .iter()
-        .enumerate()
-        .filter(|(_, value)| **value == 1)
-        .map(|(coord, _)| {
-            (
-                coord as isize / input.shape()[1] as isize,
-                coord as isize % input.shape()[1] as isize,
-                0,
-            )
-        })
-        .collect::<HashSet<Coord3>>();
-
-    for _ in 0..6 {
-        let mut next_active_cubes = HashSet::<Coord3>::new();
-
-        for active in active_cubes.iter() {
-            let neighbour_cubes = neighbours3(active);
-            let mut neighbour_cubes_active = 0;
-
-            for neighbour_cube in neighbour_cubes {
                 let neighbour_is_active = active_cubes.contains(&neighbour_cube);
 
                 if neighbour_is_active {
                     neighbour_cubes_active += 1;
                 }
 
-                let neighbour_neighbour_cubes = neighbours3(&neighbour_cube);
+                let neighbour_neighbour_cubes = neighbour_cube.neighbours();
                 let active_neighbour_neighbours = neighbour_neighbour_cubes
                     .iter()
-                    .filter(|x| active_cubes.contains(x))
+                    .filter(|x| active_cubes.contains(*x))
                     .count();
 
                 if !neighbour_is_active && active_neighbour_neighbours == 3 {
@@ -98,59 +95,7 @@ pub fn a(input: &Array2<usize>) -> usize {
             }
 
             if neighbour_cubes_active == 2 || neighbour_cubes_active == 3 {
-                next_active_cubes.insert(*active);
-            }
-        }
-        active_cubes = next_active_cubes;
-    }
-
-    return active_cubes.len();
-}
-
-// Rust doesn't support generic tuples, so copy paste it is..
-// TODO: create a generic wrapper type that implements a `neighbours` trait
-pub fn b(input: &Array2<usize>) -> usize {
-    let mut active_cubes = input
-        .iter()
-        .enumerate()
-        .filter(|(_, value)| **value == 1)
-        .map(|(coord, _)| {
-            (
-                coord as isize / input.shape()[1] as isize,
-                coord as isize % input.shape()[1] as isize,
-                0,
-                0,
-            )
-        })
-        .collect::<HashSet<Coord4>>();
-
-    for _ in 0..6 {
-        let mut next_active_cubes = HashSet::<Coord4>::new();
-
-        for active in active_cubes.iter() {
-            let neighbour_cubes = neighbours4(active);
-            let mut neighbour_cubes_active = 0;
-
-            for neighbour_cube in neighbour_cubes {
-                let neighbour_is_active = active_cubes.contains(&neighbour_cube);
-
-                if neighbour_is_active {
-                    neighbour_cubes_active += 1;
-                }
-
-                let neighbour_neighbour_cubes = neighbours4(&neighbour_cube);
-                let active_neighbour_neighbours = neighbour_neighbour_cubes
-                    .iter()
-                    .filter(|x| active_cubes.contains(x))
-                    .count();
-
-                if !neighbour_is_active && active_neighbour_neighbours == 3 {
-                    next_active_cubes.insert(neighbour_cube);
-                }
-            }
-
-            if neighbour_cubes_active == 2 || neighbour_cubes_active == 3 {
-                next_active_cubes.insert(*active);
+                next_active_cubes.insert(active.clone());
             }
         }
         active_cubes = next_active_cubes;
@@ -165,13 +110,13 @@ mod test {
 
     #[test]
     fn example_a() {
-        let result = a(&get_input().unwrap());
+        let result = a(&get_input().unwrap(), 3);
         assert_eq!(result, 317);
     }
 
     #[test]
     fn example_b() {
-        let result = b(&get_input().unwrap());
+        let result = a(&get_input().unwrap(), 4);
         assert_eq!(result, 1692);
     }
 }
